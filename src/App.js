79836ebb4422996291a830853fd10fac4076f7d4
myplancash <myplancash@gmail.com>
2022-01-10 19:52:23 -0500

    handling some npm issues and dependencies errors

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import HomePage from "./pages/homepage/homepage.component"
 import './App.css'
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss'
 
-
 class Directory extends React.Component {
   constructor() {
     super()
