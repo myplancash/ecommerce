46e40ad10d23cfe7f9a386c3c19a7be5c96e9476
myplancash <myplancash@gmail.com>
2022-01-11 22:05:23 -0500

    Updating our App with Shop Page, Collection Preview component and Collection Item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { Switch, Route } from "react-router-dom";
 
 import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
 
-const HatsPage = () => (
-  <div>
-    Hats Page to show hats
-  </div>
-)
-
 const HomePage = () => (
-  <div className="homepage">
-    <Switch>
-      <Route exact path="/" component={Directory} />
-      <Route exact path="/shop/hats" component={HatsPage} />
-    </Switch>
+  <div className='homepage'>
+    <Directory />
   </div>
-)
+);
 
 export default HomePage;
