9742ae4ab01f06a1fc1c4659db47bde8d374eabe
sergio-estebitan <myplancash@gmail.com>
2022-01-31 19:01:57 -0500

    Added a prop to cart reducer to populate a cartItems array and a new addItem action creator and used it accordingly

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSingIn, ...otherProps}) => (
-  <button className={`${ isGoogleSingIn ? 'google-sign-in' : ''} custom-button`}
+const CustomButton = ({children, isGoogleSingIn, inverted, ...otherProps}) => (
+  <button className={`${ inverted ? 'inverted' : ''} ${ isGoogleSingIn ? 'google-sign-in' : ''} custom-button`}
     {...otherProps}
   >
     {children}
