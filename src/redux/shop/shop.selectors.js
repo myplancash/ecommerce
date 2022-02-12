a31d162c26c20ceef95cb41e2de6ad080879e3e1
sergio-estebitan <myplancash@gmail.com>
2022-02-11 20:04:44 -0500

    moved and updating corresponding components to our redux store

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,6 +8,11 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+export const selectCollectionForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map(key => collections[key])
+)
+
 //to memoize selectCollection we actually have to memoize the whole function using a memoize helper function
 //We can leverage the lodash library
 // By wrapping this function is memoize, we're saying that whenever this function gets called and receives collectionUrlParam, I want to memoize the return of this function (in this case we return a selector
